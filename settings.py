80123a87b7ff499c466b9772093af826479857cd
Daniel Jones <daniel@jones.org.uk>
2012-12-06 15:29:01 +0000

    switched from tweepy to python-twitter-tools added support for archiving retweets minor improvements to URL shortening and diagnostic output

--- settings.py
+++ settings.py
@@ -11,14 +11,23 @@ archive_attributes = [ "id", "created_at", "retweet_count", "text_ascii" ]
 
 # delay between fetching individual pages of tweets
 wait = 2.0
+per_page = 50
+
+# include RTs by other people in the archive?
+include_rts = True
 
 # short-URLs using the following hostnames will be resolved
 # (note that this may make your archived tweets longer than 140 characters).
 # to disable, set this to an empty list, i.e. []
-resolve_shorturls = ["t.co", "bit.ly", "bbc.in", "wp.me", "is.gd"]
+resolve_shorturls = [ "t.co", "bit.ly", "bbc.in", "wp.me", "is.gd", "fb.me", "zite.to", "ow.ly", "goo.gl", "deck.ly" ]
+
+# by default, refuse to save partial archive results (after exiting the
+# process early) to prevent gaps in archives. 
+save_partial = True
 
 # oauth settings
 # needed for protected accounts, and to avoid load throttling
+# obtain these by creating an application at https://dev.twitter.com/apps
 consumer_key = None
 consumer_secret = None
 oauth_key = None
--- tweetarchive.py
+++ tweetarchive.py
@@ -3,7 +3,7 @@
 """
 tweetarchive:
 a lightweight python twitter archiver, producing pure ASCII output in CSV format.
-requires tweepy, and BeautifulSoup for ASCII/entity conversion.
+requires python-twitter-tools, and BeautifulSoup for ASCII/entity conversion.
 """
 __version__ = '0.0.1'
 __author__ = 'Daniel Jones'
@@ -15,16 +15,19 @@ import re
 import csv
 import sys
 import time
-import tweepy
 import pprint
 import getopt
 import httplib
+import twitter
 import urlparse
 import settings
+import email.utils 
+
 try:
 	from BeautifulSoup import BeautifulStoneSoup
 except:
 	print "couldn't import BeautifulStoneSoup (no ASCII or HTML entity conversion available)"
+	sys.exit(1)
 
 resolve_shorturl_regexes = map(lambda host: re.compile(r"https?://%s/[\w-]+" % host.replace('.', '\\.')), settings.resolve_shorturls)
 
@@ -39,81 +42,111 @@ def main(args):
 	# if we have previous tweets, check the last ID we fetched --
 	# only want to pull new ones up to this point.
 	settings.archive_last_id = None
+	settings.archive_count = 0
 	if os.path.exists(settings.archive_filename) and "id" in settings.archive_attributes:
 		try:
 			fd = open(settings.archive_filename, "r")
 			reader = csv.reader(fd)
-			row = list(reader)[-1]
+			lines = list(reader)
+			row = lines[-1]
+			settings.archive_count = len(lines)
 			settings.archive_last_id = row[settings.archive_attributes.index("id")]
-			print " - existing archive found, seeking up to ID %s" % settings.archive_last_id
+			print "existing archive found, seeking up to ID %s" % settings.archive_last_id
 			fd.close()
 		except:
 			pass
 
 	api = twitter_connect()
-	statuses = twitter_statuses(api, settings.archive_username, last_id = settings.archive_last_id, wait = settings.wait)
+	statuses = twitter_statuses(api, settings.archive_username, min_id = settings.archive_last_id, wait = settings.wait, histsize = settings.archive_count)
 
 	fd = open(settings.archive_filename, "a")
 	writer = csv.writer(fd)
 	for status in statuses:
-		row = tuple(map(lambda attribute: getattr(status, attribute), settings.archive_attributes))
+		row = tuple(map(lambda attribute: status[attribute], settings.archive_attributes))
 		writer.writerow(row)
+
 	fd.close()
 
 def twitter_connect():
-	""" create our API object via tweepy """
+	""" create our API object via python-twitter-tools,
+	    authenticating with oAuth if necessary. """
 	if settings.consumer_key is not None:
-		auth = tweepy.OAuthHandler(settings.consumer_key, settings.consumer_secret)
-		auth.set_access_token(settings.oauth_key, settings.oauth_secret)
-		api = tweepy.API(auth)
+		auth = twitter.OAuth(settings.oauth_key, settings.oauth_secret, settings.consumer_key, settings.consumer_secret)
+		api = twitter.Twitter(auth = auth)
 	else:
-		api = tweepy.API()
+		api = twitter.Twitter()
 
 	return api
 
-def twitter_statuses(api, user = "ideoforms", count = float('inf'), last_id = None, wait = 1):
-	""" iteratively pull statuses from twitter, up to a limit of <count>.
-	    wait <wait> seconds between each page to be a good citizen. """
+def twitter_statuses(api, username = "ideoforms", histsize = 0, min_id = None, wait = 1):
+	""" Iteratively pull statuses from twitter, up to a limit of <count>.
+	    Wait <wait> seconds between each page to be a good citizen. """
+
 	page = 1
+	last_id = None
 	statuses = []
-	user_info = api.get_user(user)
-	statuses_count = user_info.statuses_count
-	count = statuses_count if statuses_count < count else count
-	print "fetching %d of %d statuses" % (count, statuses_count)
+	user_info = api.users.show(screen_name = username)
+	total_count = user_info["statuses_count"]
+	fetch_count = total_count - histsize
+	print "fetching %d of %d statuses" % (fetch_count, total_count)
+
 	try:
-		while len(statuses) < count:
+		found_last = False
+		while len(statuses) < fetch_count:
 			try:
-				results = api.user_timeline(user, page = page)
+				if last_id:
+					results = api.statuses.user_timeline(screen_name = username, count = settings.per_page, include_rts = settings.include_rts, max_id = last_id - 1)
+				else:
+					results = api.statuses.user_timeline(screen_name = username, count = settings.per_page, include_rts = settings.include_rts)
 			except:
 				print " - fetch failed (might have hit request limit, try using oauth settings)"
 				break
 
 			if not results:
-				break
+				print " - no results (not sure why)"
+				time.sleep(2)
 
-			print " - got page %d (%d/%d statuses)" % (page, len(statuses) + len(results), statuses_count)
+			cumulative_count = len(statuses) + len(results)
+			if cumulative_count > fetch_count:
+				cumulative_count = fetch_count
+			print " - got page %d (%d/%d statuses)" % (page, cumulative_count, fetch_count)
+			page += 1
 
 			for status in results:
-				# pprint.pprint(vars(status))
-				if last_id is not None and str(status.id) == last_id:
-					count = 0
+				if min_id is not None and str(status["id"]) == min_id:
+					found_last = True
 					break
+
 				try:
-					unescaped = unicode(BeautifulStoneSoup(status.text, convertEntities = BeautifulStoneSoup.HTML_ENTITIES))
+					unescaped = unicode(BeautifulStoneSoup(status["text"], convertEntities = BeautifulStoneSoup.HTML_ENTITIES))
 					ascii = unescaped.encode('ascii', 'ignore')
+					ascii = ascii.replace("\n", " ")
 					ascii = resolve_urls(ascii)
-					status.text_ascii = ascii
-				except:
+					status["text_ascii"] = ascii
+				except Exception, e:
+					print "couldn't parse tweet: %s (%s)" % (status["text"], e)
 					pass
 
+				# parse ISO-8601 date
+				timestamp = email.utils.parsedate(status["created_at"])
+				status["created_at"] = time.strftime("%Y-%m-%d %H:%M:%S", timestamp)
+
+				if settings.verbose:
+					print "%d (%s) %s" % (status["id"], status["created_at"], status["text_ascii"])
+
+				last_id = status["id"]
 				statuses.append(status)
 
-			page += 1
+			if found_last:
+				break
+
 			time.sleep(wait)
 
 	except KeyboardInterrupt:
-		print "terminated (fetched %d/%d statuses)" % (len(statuses), statuses_count)
-		pass
+		print "terminated (fetched %d/%d statuses)" % (len(statuses), fetch_count)
+		if not settings.save_partial:
+			print "(not saving to disk to avoid gaps in archive)"
+			sys.exit(1)
 
 	print "finished (found %d new tweets)" % len(statuses)
 
@@ -121,54 +154,77 @@ def twitter_statuses(api, user = "ideoforms", count = float('inf'), last_id = No
 	return statuses
 
 def resolve_urls(string):
-	"""Searches a string for URLs on a specific hostname (usually link-shorterning URLs)
-	and tries to resolve them, returning a modified string."""
+	""" Searches a string for URLs on a specific hostname (usually link-shorterning URLs)
+	    and tries to resolve them, returning a modified string. """
 	for re in resolve_shorturl_regexes:
-		while True:
-			amatch = re.search(string)
-			if not amatch: break
-			urlfrm = string[amatch.start():amatch.end()]
-			urltoo = urlfrm
-			iterdepth = 0
-			while any(re.match(urltoo) for re in resolve_shorturl_regexes):
-				iterdepth += 1
-				if iterdepth == 10:
-					print "Warning: too many redirects, giving up resolving %s" % urlfrm
-					return string
-				# resolve the HTTP redirect
-				o = urlparse.urlparse(urltoo, allow_fragments=True)
-				conn = httplib.HTTPConnection(o.netloc)
-				path = o.path
-				if o.query:
-					path +='?'+o.query
-				conn.request("HEAD", path)
-				res = conn.getresponse()
-				headers = dict(res.getheaders())
-				urltoo = headers['location']
-			#print "Resolved %s -> %s" % (urlfrm, urltoo)
-			string = string.replace(urlfrm, urltoo)
+		try:
+			while True:
+				amatch = re.search(string)
+				if not amatch:
+					break
+
+				urlfrm = string[amatch.start():amatch.end()]
+				urltoo = urlfrm
+				iterdepth = 0
+				while any(re.match(urltoo) for re in resolve_shorturl_regexes):
+					iterdepth += 1
+					if iterdepth == 10:
+						print "Warning: too many redirects, giving up resolving %s" % urlfrm
+						return string
+
+					# resolve the HTTP redirect
+					o = urlparse.urlparse(urltoo, allow_fragments = True)
+					conn = httplib.HTTPConnection(o.netloc)
+					path = o.path
+					if o.query:
+						path += '?' + o.query
+					conn.request("HEAD", path)
+					res = conn.getresponse()
+					headers = dict(res.getheaders())
+					urltoo = headers['location']
+
+				# print "Resolved %s -> %s" % (urlfrm, urltoo)
+				string = string.replace(urlfrm, urltoo)
+		except:
+			# possible error resolving URL; leave it as is
+			pass
+
 	return string
 
 if __name__ == "__main__":
 	""" first, parse command-line arguments """
 	args = sys.argv[1:]
 
-	opts, args = getopt.getopt(args, "hu:f:w:")
+	opts, args = getopt.getopt(args, "hvu:f:w:")
 	for key, value in opts:
 		if key == "-h":
-			print "Usage: %s [-h] [-u <username>] [-f <filename>] [-w <wait>]" % (sys.argv[0])
+			print "Usage: %s [-hv] [-u <username>] [-f <filename>] [-w <wait>]" % (sys.argv[0])
 			print ""
+			print "  -v  verbose output"
 			print "  -h  show this help"
 			print "  -u  username to archive"
 			print "  -f  filename to archive to"
 			print "  -w  delay time between fetching each page"
 			sys.exit(1)
+		elif key == "-v":
+			settings.verbose = True
 		elif key == "-u":
 			settings.archive_username = value
 		elif key == "-f":
 			settings.archive_filename = value
 		elif key == "-w":
 			settings.wait = float(value)
+
+	if not hasattr(settings, "wait_time"):
+		settings.wait = 2.0
+	if not hasattr(settings, "per_page"):
+		settings.per_page = 50
+	if not hasattr(settings, "include_rts"):
+		settings.include_rts = True
+	if not hasattr(settings, "verbose"):
+		settings.verbose = False
+	if not hasattr(settings, "save_partial"):
+		settings.save_partial = False
 	
 	main(args)
 
